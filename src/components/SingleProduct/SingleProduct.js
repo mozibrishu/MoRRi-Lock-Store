1e781b2d2914fa74995ed05818e17ee3955fb679
mozibrishu <mozib.diu@gmail.com>
2021-04-15 20:39:41 +0600

    delete complete

--- src/components/SingleProduct/SingleProduct.js
+++ src/components/SingleProduct/SingleProduct.js
@@ -2,11 +2,24 @@ import React from 'react';
 
 const SingleProduct = (props) => {
     const { _id, name, price, imageURL } = props.product;
+
+    function deleteProduct(event, id) {
+        fetch(`http://localhost:5000/delete/${id}`, {
+            method: 'DELETE'
+        })
+            .then(res => res.json())
+            .then(result => {
+                if (result) {
+                    event.target.parentNode.parentNode.style.display = 'none';
+                }
+            })
+    }
+
     return (
         <tr>
             <td>{name}</td>
             <td>${price || 0}</td>
-            <td><button className="btn btn-danger">Delete</button></td>
+            <td><button className="btn btn-danger" onClick={(event) => deleteProduct(event,`${_id}`)}>Delete</button></td>
         </tr>
     );
 };
